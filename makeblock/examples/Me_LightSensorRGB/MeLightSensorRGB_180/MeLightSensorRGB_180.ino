e19edd42aeaca0827e12a5bbd686fa53af7170c6
Payton <Payton.liang@makeblock.com>
2017-10-26 15:19:21 +0800

    for linght sensor RGB

---
+++ makeblock/examples/Me_LightSensorRGB/MeLightSensorRGB_180/MeLightSensorRGB_180.ino
@@ -0,0 +1,148 @@
+#include <Arduino.h>
+#include <Wire.h>
+#include <SoftwareSerial.h>
+
+#include <MeMegaPiPro.h>
+
+
+MeEncoderOnBoard Encoder_1(SLOT1);
+MeEncoderOnBoard Encoder_2(SLOT2);
+MeEncoderOnBoard Encoder_3(SLOT3);
+MeEncoderOnBoard Encoder_4(SLOT4);
+Me4Button btn(PORT_8);
+MeLightSensorRGB LightSensorRGB;
+
+uint8_t keyPressed = KEY_NULL;
+uint8_t keyPressedPrevious = KEY_NULL;
+int16_t turnoffset = 0;
+uint8_t sensorstate[4];
+int16_t set_speed = 0;
+uint8_t run_flag = 0;
+uint8_t study_flag = 0;
+unsigned long motor_ctrl_time = 0;
+unsigned long printf_time = 0;
+
+void isr_process_encoder1(void)
+{
+    if(digitalRead(Encoder_1.getPortB()) == 0){
+        Encoder_1.pulsePosMinus();
+    }else{
+        Encoder_1.pulsePosPlus();
+    }
+}
+void isr_process_encoder2(void)
+{
+    if(digitalRead(Encoder_2.getPortB()) == 0){
+        Encoder_2.pulsePosMinus();
+    }else{
+        Encoder_2.pulsePosPlus();
+    }
+}
+void isr_process_encoder3(void)
+{
+    if(digitalRead(Encoder_3.getPortB()) == 0){
+        Encoder_3.pulsePosMinus();
+    }else{
+        Encoder_3.pulsePosPlus();
+    }
+}
+void isr_process_encoder4(void)
+{
+    if(digitalRead(Encoder_4.getPortB()) == 0){
+        Encoder_4.pulsePosMinus();
+    }else{
+        Encoder_4.pulsePosPlus();
+    }
+}
+
+
+
+void setup(){
+    TCCR1A = _BV(WGM10);//PIN12
+    TCCR1B = _BV(CS11) | _BV(CS10) | _BV(WGM12);
+    
+    TCCR2A = _BV(WGM21) | _BV(WGM20);//PIN8
+    TCCR2B = _BV(CS22);
+    
+    TCCR3A = _BV(WGM30);//PIN9
+    TCCR3B = _BV(CS31) | _BV(CS30) | _BV(WGM32);
+    
+    TCCR4A = _BV(WGM40);//PIN5
+    TCCR4B = _BV(CS41) | _BV(CS40) | _BV(WGM42);
+    
+    attachInterrupt(Encoder_3.getIntNum(), isr_process_encoder3, RISING);
+    attachInterrupt(Encoder_4.getIntNum(), isr_process_encoder4, RISING);
+
+		LightSensorRGB.begin();
+		LightSensorRGB.setKp(0.18);
+    Serial.begin(115200);
+}
+
+void loop()
+{
+	
+  keyPressedPrevious = keyPressed;
+  keyPressed = btn.pressed();
+  if (keyPressedPrevious != keyPressed)
+  {
+    if (keyPressed == KEY_3)
+    {
+    	run_flag = 1;
+      Serial.println("KEY3 pressed");
+    }
+    if (keyPressed == KEY_4)
+    {
+    	run_flag = 0;
+      Serial.println("KEY4 pressed");
+    }
+    if (keyPressed == KEY_NULL)
+    {
+      Serial.println("KEY NULL");
+    }
+  }
+	
+	  if(millis() - motor_ctrl_time > 10)  
+	  { 
+	   	study_flag = LightSensorRGB.getStudyFlag();
+	  		
+	  	if((run_flag == 1) && (study_flag == 0))
+			{
+			  motor_ctrl_time = millis(); 
+				LightSensorRGB.getPositionState(sensorstate, 4);
+				turnoffset = LightSensorRGB.getPositionOffset();
+			  if(set_speed < 100)
+			  {
+					set_speed++;
+			  }
+				Encoder_3.setTarPWM(set_speed + turnoffset);
+	    	Encoder_4.setTarPWM(-(set_speed - turnoffset));
+			}
+		  else
+		  {
+		  	turnoffset = 0;
+		  	set_speed = 0;
+		  	run_flag = 0;
+		 		Encoder_3.setTarPWM(0);
+		    Encoder_4.setTarPWM(-0);
+		  }
+  }
+
+  if(millis() - printf_time > 500)  
+  {    
+	  printf_time = millis();  
+	  Serial.print(sensorstate[0]);
+	  Serial.print(", ");
+	  Serial.print(sensorstate[1]);
+	  Serial.print(", ");
+	  Serial.print(sensorstate[2]);
+	  Serial.print(", ");
+	  Serial.print(sensorstate[3]);
+	  Serial.print(", ");
+ 		Serial.println(turnoffset);
+  }
+    
+	Encoder_3.loop();
+	Encoder_4.loop();
+}
+
+
---
+++ makeblock/src/MeLightSensorRGB.cpp
@@ -0,0 +1,584 @@
+/**
+ * \par Copyright (C), 2012-2016, MakeBlock
+ * \class   MeLightSensorRGB
+ * \brief   Driver for MeLightSensorRGB module.
+ * @file    MeLightSensorRGB.cpp
+ * @author  MakeBlock
+ * @version V1.0.0
+ * @date    2017/09/26
+ * @brief   Driver for MeLightSensorRGB module.
+ *
+ * \par Copyright
+ * This software is Copyright (C), 2012-2016, MakeBlock. Use is subject to license \n
+ * conditions. The main licensing options available are GPL V2 or Commercial: \n
+ *
+ * \par Open Source Licensing GPL V2
+ * This is the appropriate option if you want to share the source code of your \n
+ * application with everyone you distribute it to, and you also want to give them \n
+ * the right to share who uses it. If you wish to use this software under Open \n
+ * Source Licensing, you must contribute all your source code to the open source \n
+ * community in accordance with the GPL Version 2 when your application is \n
+ * distributed. See http://www.gnu.org/copyleft/gpl.html
+ *
+ * \par Description
+ * This file is a drive for MeLightSensorRGB module, It supports MeLightSensorRGB V1.0 device provided
+ * by MakeBlock.
+ *
+ * \par Method List:
+ *
+ *    1. void MeLightSensorRGB::setpin(uint8_t AD0, uint8_t INT)
+ *    2. void MeLightSensorRGB::begin(void)
+ *    3. void MeLightSensorRGB::update(void)
+ *    4. void MeLightSensorRGB::fast_update(void)
+ *    5. uint8_t MeLightSensorRGB::getDevAddr(void)
+ *    6. double MeLightSensorRGB::getAngleX(void)
+ *    7. double MeLightSensorRGB::getAngleY(void)
+ *    8. double MeLightSensorRGB::getAngleZ(void)
+ *    9. double MeLightSensorRGB::getGyroX(void)
+ *    10. double MeLightSensorRGB::getGyroY(void)
+ *
+ * \par History:
+ * <pre>
+ * `<Author>`         `<Time>`        `<Version>`        `<Descr>`
+ *  Payton            2017/09/26        1.0.0         rebuild the old lib.
+ * </pre>
+ *
+ * @example MeLightSensorRGBTest.ino
+ */
+
+/* Includes ------------------------------------------------------------------*/
+#include "MeLightSensorRGB.h"
+
+  
+//ËÀÇøÏÞÖÆ
+#define LIMIT_DEATH(x, a)	if( (((x) > 0) && ((x) < (a))) ) (x) = 0;\
+            							else if((x) >= (a)) (x) = (x) - (a);\
+            							else if( (((x) <= 0) && ((x) >= -(a))) ) (x) = 0;	\
+            							else (x) = (x) + (a);
+
+
+/* Private functions ---------------------------------------------------------*/
+#ifdef ME_PORT_DEFINED
+/**
+ * Alternate Constructor which can call your own function to map the MeLightSensorRGB to arduino port,
+ * no pins are used or initialized here
+ */
+MeLightSensorRGB::MeLightSensorRGB(void) : MePort(0)
+{
+  Device_Address = LIGHTSENSORRGB_DEFAULT_ADDRESS;
+}
+
+/**
+ * Alternate Constructor which can call your own function to map the MeLightSensorRGB to arduino port,
+ * no pins are used or initialized here, but PWM frequency set to 976 Hz
+ * \param[in]
+ *   port - RJ25 port from PORT_1 to M2
+ */
+MeLightSensorRGB::MeLightSensorRGB(uint8_t port) : MePort(port)
+{
+  Device_Address = LIGHTSENSORRGB_DEFAULT_ADDRESS;
+}
+
+/**
+ * Alternate Constructor which can call your own function to map the MeLightSensorRGB to arduino port
+ * and change the i2c device address
+ * no pins are used or initialized here, but PWM frequency set to 976 Hz
+ * \param[in]
+ *   port - RJ25 port from PORT_1 to M2
+ * \param[in]
+ *   address - the i2c address you want to set
+ */
+MeLightSensorRGB::MeLightSensorRGB(uint8_t port, uint8_t address) : MePort(port)
+{
+  Device_Address = address;
+}
+#else  // ME_PORT_DEFINED
+/**
+ * Alternate Constructor which can call your own function to map the _AD0 and _INT to arduino port,
+ * no pins are used or initialized here
+ * \param[in]
+ *   _AD0 - arduino gpio number
+ * \param[in]
+ *   _INT - arduino gpio number
+ */
+MeLightSensorRGB::MeLightSensorRGB(uint8_t AD0, uint8_t INT)
+{
+  Device_Address = LIGHTSENSORRGB_DEFAULT_ADDRESS;
+  _AD0 = AD0;
+  _INT = INT;
+}
+
+/**
+ * Alternate Constructor which can call your own function to map the _AD0 and _INT to arduino port
+ * and change the i2c device address, no pins are used or initialized here
+ * \param[in]
+ *   _AD0 - arduino gpio number
+ * \param[in]
+ *   _INT - arduino gpio number
+ * \param[in]
+ *   address - the i2c address you want to set
+ */
+MeLightSensorRGB::MeLightSensorRGB(uint8_t AD0, uint8_t INT, uint8_t address)
+{
+  Device_Address = address;
+  _AD0 = AD0;
+  _INT = INT;
+}
+#endif // ME_PORT_DEFINED
+
+/**
+ * \par Function
+ *   setpin
+ * \par Description
+ *   Set the PIN of the button module.
+ * \param[in]
+ *   AD0 - pin mapping for arduino
+ * \param[in]
+ *   INT - pin mapping for arduino
+ * \par Output
+ *   None
+ * \return
+ *   None.
+ * \par Others
+ *   Set global variable _AD0, _INT, s1 and s2
+ */
+void MeLightSensorRGB::setpin(uint8_t AD0, uint8_t INT)
+{
+  _AD0 = AD0;
+  _INT = INT;
+#ifdef ME_PORT_DEFINED
+  s1 = AD0;
+  s2 = INT;
+#endif // ME_PORT_DEFINED
+}
+
+/**
+ * \par Function
+ *   begin
+ * \par Description
+ *   Initialize the MeLightSensorRGB.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   None
+ * \par Others
+ *   
+ */
+void MeLightSensorRGB::begin(void)
+{
+  uint8_t i;
+  
+  for(i=0; i<LIGHTSENSOR_RGB_NUM; i++)
+  {
+
+  }
+  Kp = 0.5;
+  study_flag = false;
+  
+  Wire.begin();
+  delay(100);
+}
+
+/**
+ * \par Function
+ *   getDevAddr
+ * \par Description
+ *   Get the device address of Gyro.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   The device address of Gyro
+ * \par Others
+ *   None
+ */
+uint8_t MeLightSensorRGB::getDevAddr(void)
+{
+  return Device_Address;
+}
+
+/**
+ * \par Function
+ *   getADCValueS1
+ * \par Description
+ *   Get the S1 sensor ADC value.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   The ADC value of S1
+ * \par Others
+ *   
+ */
+uint8_t MeLightSensorRGB::getADCValueS1(void)
+{
+  int8_t return_value;
+  /* read data */
+  return_value = readData(LIGHTSENSOR_RGB_DATA_S1, &i2cData[0], 1);
+  if(return_value == I2C_OK)
+  {
+    adcOutput[0] = i2cData[0];
+  }
+
+  return adcOutput[0];
+}
+
+/**
+ * \par Function
+ *   getADCValueS2
+ * \par Description
+ *   Get the S2 sensor ADC value.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   The ADC value of S2
+ * \par Others
+ *   
+ */
+uint8_t MeLightSensorRGB::getADCValueS2(void)
+{
+  int8_t return_value;
+  /* read data */
+  return_value = readData(LIGHTSENSOR_RGB_DATA_S2, &i2cData[1], 1);
+  if(return_value == I2C_OK)
+  {
+    adcOutput[1] = i2cData[1];
+  }
+
+  return adcOutput[1];
+
+}
+
+/**
+ * \par Function
+ *   getADCValueS3
+ * \par Description
+ *   Get the S3 sensor ADC value.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   The ADC value of S3
+ * \par Others
+ *   
+ */
+uint8_t MeLightSensorRGB::getADCValueS3(void)
+{
+  int8_t return_value;
+  /* read data */
+  return_value = readData(LIGHTSENSOR_RGB_DATA_S3, &i2cData[2], 1);
+  if(return_value == I2C_OK)
+  {
+    adcOutput[2] = i2cData[2];
+  }
+
+  return adcOutput[2];
+
+}
+
+/**
+ * \par Function
+ *   getADCValueS4
+ * \par Description
+ *   Get the S4 sensor ADC value.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   The ADC value of S4
+ * \par Others
+ *   
+ */
+uint8_t MeLightSensorRGB::getADCValueS4(void)
+{
+  int8_t return_value;
+  /* read data */
+  return_value = readData(LIGHTSENSOR_RGB_DATA_S4, &i2cData[3], 1);
+  if(return_value == I2C_OK)
+  {
+    adcOutput[3] = i2cData[3];
+  }
+
+  return adcOutput[3];
+}
+
+/**
+ * \par Function
+ *   getAllADCValue
+ * \par Description
+ *   Get the S1¡¢S2¡¢S3¡¢S4 sensor ADC value.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   None
+ * \par Others
+ *  
+ */
+void MeLightSensorRGB::getAllADCValue(uint8_t *adcbuff, uint8_t size)
+{
+  int8_t return_value;
+      
+  /* read data */
+  return_value = readData(LIGHTSENSOR_RGB_DATA_S1, i2cData, size);
+  if(return_value == I2C_OK)
+  {
+    memcpy(adcOutput, i2cData, size);
+  }
+
+  memcpy(adcbuff, adcOutput, size);
+}
+
+/**
+ * \par Function
+ *   setKp
+ * \par Description
+ *   set Kp.
+ * \param[in]
+ *   None
+ * \par Output
+ *   None
+ * \return
+ *   None
+ * \par Others
+ *  
+ */
+void MeLightSensorRGB::setKp(float value)
+{
+  Kp = value;
+}
+
+/**
+ * \par Function
+ *   getPositionOffset
+ * \par Description
+ *   get turn Value.
+ * \param[in]
+ *   Speed:set forward speed
+ * \par Output
+ *   None
+ * \return
+ *   Position offset Value,-512 ~ 512
+      <0,in right
+      >0,in left
+ * \par Others
+ *   |--------------------------|
+     |     |      |      |      |
+     |  S1 |  S2  |  S3  |  S4  |
+     |     |      |      |      |
+     |--------------------------|
+ */
+int16_t MeLightSensorRGB::getPositionOffset(void)
+{
+  int8_t return_value;
+  int16_t outvalue;
+  static int16_t pre_outvalue;
+
+  /* read data */
+  return_value = readData(LIGHTSENSOR_RGB_TURNOFFSET_L, &i2cData[8], 2);
+  if(return_value == I2C_OK)
+  {
+    pre_outvalue = (int16_t)(i2cData[8] | ((uint16_t)i2cData[9]<<8));
+  }
+
+  outvalue = (int16_t)(Kp * pre_outvalue);
+  outvalue = constrain(outvalue, -512, 512);
+  return outvalue;
+}
+
+/**
+ * \par Function
+ *   getPositionState
+ * \par Description
+ *   get turn Value.
+ * \param[in]
+ *   Speed:set forward speed
+ * \par Output
+ *   None
+ * \return
+ *   Position offset Value,-255 ~ 255
+      <0,in right
+      >0,in left
+ * \par Others
+ *   |--------------------------|
+     |     |      |      |      |
+     |  S1 |  S2  |  S3  |  S4  |
+     |     |      |      |      |
+     |--------------------------|
+ */
+void MeLightSensorRGB::getPositionState(uint8_t *statebuff, uint8_t num)
+{
+  int8_t return_value;
+  
+  if(num <= LIGHTSENSOR_RGB_NUM)  
+  {
+    /* read data */
+    return_value = readData(LIGHTSENSOR_RGB_STATE_S1, &i2cData[4], LIGHTSENSOR_RGB_NUM);
+    if(return_value == I2C_OK)
+    {
+      memcpy(statebuff, &i2cData[4], num);
+    }
+  }
+}
+
+/**
+ * \par Function
+ *   getStudyFlag
+ * \par Description
+ *   get turn Value.
+ * \param[in]
+ *   Speed:set forward speed
+ * \par Output
+ *   None
+ * \return
+ *   
+ * \par Others
+ */
+bool MeLightSensorRGB::getStudyFlag(void)
+{
+  int8_t return_value;
+  static uint8_t pre_value = 0;
+
+  /* read data */
+  return_value = readData(LIGHTSENSOR_RGB_STUDY_FLAG, &i2cData[10], 1);
+  if(return_value == I2C_OK)
+  {
+    pre_value = i2cData[10];
+  }
+  study_flag = pre_value;
+  return study_flag;
+}
+
+/**
+ * \par Function
+ *   writeReg
+ * \par Description
+ *   Write the registor of i2c device.
+ * \param[in]
+ *   reg - the address of registor.
+ * \param[in]
+ *   data - the data that will be written to the registor.
+ * \par Output
+ *   None
+ * \return
+ *   Return the error code.
+ *   the definition of the value of variable return_value:
+ *   0:success
+ *   1:BUFFER_LENGTH is shorter than size
+ *   2:address send, nack received
+ *   3:data send, nack received
+ *   4:other twi error
+ *   refer to the arduino official library twi.c
+ * \par Others
+ *   To set the registor for initializing.
+ */
+int8_t MeLightSensorRGB::writeReg(int16_t reg, uint8_t data)
+{
+  int8_t return_value = 0;
+  return_value = writeData(reg, &data, 1);
+  return(return_value);
+}
+
+/**
+ * \par Function
+ *   readData
+ * \par Description
+ *   Write the data to i2c device.
+ * \param[in]
+ *   start - the address which will write the data to.
+ * \param[in]
+ *   pData - the head address of data array.
+ * \param[in]
+ *   size - set the number of data will be written to the devide.
+ * \par Output
+ *   None
+ * \return
+ *   Return the error code.
+ *   the definition of the value of variable return_value:
+ *   0:success
+ *   1:BUFFER_LENGTH is shorter than size
+ *   2:address send, nack received
+ *   3:data send, nack received
+ *   4:other twi error
+ *   refer to the arduino official library twi.c
+ * \par Others
+ *   Calling the official i2c library to read data.
+ */
+int8_t MeLightSensorRGB::readData(uint8_t start, uint8_t *buffer, uint8_t size)
+{
+  int16_t i = 0;
+  int8_t return_value = 0;
+  Wire.beginTransmission(Device_Address);
+  return_value = Wire.write(start);
+  if(return_value != 1)
+  {
+    return(I2C_ERROR);
+  }
+  return_value = Wire.endTransmission(false);
+  if(return_value != 0)
+  {
+    return(return_value);
+  }
+  delayMicroseconds(1);
+  /* Third parameter is true: relase I2C-bus after data is read. */
+  Wire.requestFrom(Device_Address, size, (uint8_t)true);
+  while(Wire.available() && i < size)
+  {
+    buffer[i++] = Wire.read();
+  }
+  delayMicroseconds(1);
+  if(i != size)
+  {
+    return(I2C_ERROR);
+  }
+  return(0); //return: no error 
+}
+
+/**
+ * \par Function
+ *   writeData
+ * \par Description
+ *   Write the data to i2c device.
+ * \param[in]
+ *   start - the address which will write the data to.
+ * \param[in]
+ *   pData - the head address of data array.
+ * \param[in]
+ *   size - set the number of data will be written to the devide.
+ * \par Output
+ *   None
+ * \return
+ *   Return the error code.
+ *   the definition of the value of variable return_value:
+ *   0:success
+ *   1:BUFFER_LENGTH is shorter than size
+ *   2:address send, nack received
+ *   3:data send, nack received
+ *   4:other twi error
+ *   refer to the arduino official library twi.c
+ * \par Others
+ *   Calling the official i2c library to write data.
+ */
+int8_t MeLightSensorRGB::writeData(uint8_t start, const uint8_t *pData, uint8_t size)
+{
+  int8_t return_value = 0;
+  Wire.beginTransmission(Device_Address);
+  return_value = Wire.write(start); 
+  if(return_value != 1)
+  {
+    return(I2C_ERROR);
+  }
+  Wire.write(pData, size);  
+  return_value = Wire.endTransmission(true); 
+  return(return_value); //return: no error                     
+}
+
